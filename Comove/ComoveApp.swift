4cc220276f14dea2232794f708e51d8682b2fad9
Alexander Kozin <mail@akozin.ru>
2022-08-20 12:11:17 +0300

    Initial Commit

---
+++ Comove/ComoveApp.swift
@@ -0,0 +1,20 @@
+//
+//  ComoveApp.swift
+//  Comove
+//
+//  Created by akozin on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ComoveApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
