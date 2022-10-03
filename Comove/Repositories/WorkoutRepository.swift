f3f777f43b76ef99679bd932a0caa85f73c51e33
Alexander Kozin <mail@akozin.ru>
2022-10-03 18:41:01 +0300

    Add repository for managing workouts
    Workouts must be saved and fetched.

---
+++ Comove/Repositories/WorkoutRepository.swift
@@ -0,0 +1,43 @@
+//
+//  WorkoutRepository.swift
+//  Comove
+//
+//  Created by akozin on 03.10.2022.
+//
+
+import Foundation
+import CoreData
+
+/// Repository, which manages saving and fetching workouts.
+protocol WorkoutRepository {
+    /// Adds workout to repository.
+    /// - Parameters:
+    ///   - duration: Workout duration in seconds.
+    ///   - distance: Workout distance in meters.
+    func addWorkout(duration: Int32, distance: Int32)
+}
+
+struct LocalWorkoutRepository: WorkoutRepository {
+    private let context: NSManagedObjectContext
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+    
+    func addWorkout(duration: Int32, distance: Int32) {
+        let workout = WorkoutEntity(context: context)
+        workout.distance = distance
+        workout.duration = duration
+        do {
+            try context.save()
+            print("Workout saved!")
+        } catch {
+            print("Workout saving error!")
+            print(error.localizedDescription)
+        }
+    }
+}
+
+struct FakeWorkoutRepository: WorkoutRepository {
+    func addWorkout(duration: Int32, distance: Int32) {}
+}
